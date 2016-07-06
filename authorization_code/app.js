e129412d357bdfff1e6529ae29b2414255982be2
Babette Hoogendoorn <babettehoogendoorn@hotmail.com>
2016-07-06 17:34:41 +0200

    First commit

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -7,21 +7,69 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
+//Models
+var Sequelize = require('sequelize');
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var pg = require('pg');
+var session = require('express-session');
+var app = express();
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+//client info
+var client_id = '42b8675a7083451fa42112a5fb9a7d27'; // Your client id
+var client_secret = '571ef69d3a714decb88d8bdbd0407947'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+//connect to server
+var sequelize = new Sequelize ('musicvote', 'babettehoogendoorn', null, {
+  host: 'localhost',
+  dialect: 'postgres',
+  define: {
+    timestamps: false
+  }
+});
+
+//set table in databse for stored information
+var User = sequelize.define('user', {
+  user_id: Sequelize.STRING,
+  user_email: Sequelize.STRING,
+  access_token: Sequelize.STRING,
+  refresh_token: Sequelize.STRING
+});
+
+var Song = sequelize.define('song', {
+  title: Sequelize.STRING,
+  artist: Sequelize.STRING
+});
+
+var Vote = sequelize.define('vote');
+
+var Event = sequelize.define('event', {
+  event_name: Sequelize.STRING,
+  event_end: Sequelize.STRING
+});
+
+//relation between tables
+Vote.hasMany(User);
+User.belongsTo(Vote);
+
+Vote.belongsTo(Song);
+Song.hasMany(Vote);
+
+Vote.belongsTo(Event);
+Event.hasMany(Vote);
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+ var generateRandomString = function(length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -35,9 +83,11 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
+//activate bodyParser
 app.use(express.static(__dirname + '/public'))
-   .use(cookieParser());
+.use(cookieParser());
 
+//get login
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
@@ -55,6 +105,7 @@ app.get('/login', function(req, res) {
     }));
 });
 
+//get callback
 app.get('/callback', function(req, res) {
 
   // your application requests refresh and access tokens
@@ -84,11 +135,22 @@ app.get('/callback', function(req, res) {
       json: true
     };
 
+
+    
+
+    // .spread(function(user, created) {
+    //   console.log(user.get({
+    //     plain: true
+    //   }))
+    //   console.log(created)
+    // })
+
+    
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+        refresh_token = body.refresh_token;
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
@@ -98,7 +160,29 @@ app.get('/callback', function(req, res) {
 
         // use the access token to access the Spotify Web API
         request.get(options, function(error, response, body) {
+          console.log('API Get Body')
           console.log(body);
+          //save userdata in database
+          User.findOrCreate({
+            where: {
+              user_id: body.id
+            }
+          }).then(function(theuser) {
+            User.findOne({
+              where: {
+                user_id: body.id
+              }
+            }).then(function(theuser){
+              theuser.update({
+                user_email: body.email,
+                access_token: access_token,
+                refresh_token: refresh_token
+              })
+              console.log('User created and updated:')
+              console.log(theuser)
+            })
+            
+          });
         });
 
         // we can also pass the token to the browser to make requests from there
@@ -117,6 +201,7 @@ app.get('/callback', function(req, res) {
   }
 });
 
+//get refresh token
 app.get('/refresh_token', function(req, res) {
 
   // requesting access token from refresh token
@@ -141,5 +226,13 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+  //tell the port to listen
+  var server = app.listen(8888, function () {
+    console.log('Example app listening on port: ' + server.address().port);
+  });
+
+
+  //synching with database
+  sequelize.sync({force: true}).then( function(){
+    console.log('sync done');
+  });
